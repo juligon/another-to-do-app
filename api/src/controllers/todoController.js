3371925d71bf141eff28b9e84e596f36dd88effe
juligon <julianagon73@gmail.com>
2023-10-25 05:16:25 -0300

    fixing bugs

--- api/src/controllers/todoController.js
+++ api/src/controllers/todoController.js
@@ -3,60 +3,61 @@ const { Op } = require("sequelize");
 
 // Función para obtener todos los To-Dos y aplicar filtro por título y categoría
 const getToDos = async (req, res, next) => {
-  
-  const title = req.query.title || '';
-	const category = req.query.category || '';
+	const { title, category } = req.query;
+	console.log("Title:", title);
+	console.log("Category:", category);
 
+	let whereClause = {};
 
-  const whereClause = buildWhereClause(title, category);
+	if (title || category) {
+		whereClause[Op.or] = [];
 
-  try {
-    const todos = await fetchToDos(whereClause);
-    handleNoToDosFound(todos, title, category);
-    res.json(todos);
-  } catch (error) {
-    next(error);
-  }
+		// Verifica si el valor de búsqueda coincide con una categoría predefinida
+		const predefinedCategories = ["urgent", "important", "later"];
+		if (predefinedCategories.includes(title)) {
+			whereClause[Op.or].push({
+				category: {
+					[Op.iLike]: `%${title}%`,
+				},
+			});
+		} else {
+			if (title) {
+				whereClause[Op.or].push({
+					title: {
+						[Op.iLike]: `%${title}%`,
+					},
+				});
+			}
+
+			if (category) {
+				whereClause[Op.or].push({
+					category: {
+						[Op.iLike]: `%${category}%`,
+					},
+				});
+			}
+		}
+	}
+
+	try {
+		const todos = await Todo.findAll({
+			where: whereClause,
+		});
+
+		if (todos.length === 0) {
+			const error = new Error(
+				`To-Dos not found for title: ${title}, category: ${category}`
+			);
+			error.status = 404;
+			throw error;
+		}
+
+		res.json(todos);
+	} catch (error) {
+		next(error);
+	}
 };
 
-const buildWhereClause = (title, category) => {
-  const whereClause = { [Op.or]: [] };
-
-  if (isPredefinedCategory(title)) {
-    whereClause[Op.or].push(buildCategorySearch(title));
-  } else {
-    if (title) whereClause[Op.or].push(buildTitleSearch(title));
-    if (category) whereClause[Op.or].push(buildCategorySearch(category));
-  }
-
-  return whereClause;
-};
-
-
-const isPredefinedCategory = (value) => {
-  const predefinedCategories = ["urgent", "important", "later"];
-  return predefinedCategories.includes(value);
-};
-
-const buildCategorySearch = (value) => ({
-  category: { [Op.iLike]: `%${value}%` },
-});
-
-const buildTitleSearch = (value) => ({
-  title: { [Op.iLike]: `%${value}%` },
-});
-
-const fetchToDos = (whereClause) => Todo.findAll({ where: whereClause });
-
-const handleNoToDosFound = (todos, title, category) => {
-  if (todos.length === 0) {
-    const error = new Error(
-      `To-Dos not found for title: ${title}, category: ${category}`
-    );
-    error.status = 404;
-    throw error;
-  }
-};
 
 // Función para buscar un To-Do por su ID
 const getToDoById = async (req, res, next) => {
