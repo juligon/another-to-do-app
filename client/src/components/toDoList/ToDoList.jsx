363bec440449abdc13be449b77c457ba7736550f
juligon <julianagon73@gmail.com>
2023-10-26 11:07:45 -0300

    fixing bugs

--- client/src/components/toDoList/ToDoList.jsx
+++ client/src/components/toDoList/ToDoList.jsx
@@ -59,8 +59,12 @@ export default function ToDoList({ todos: initialTodos }) {
 	}, [initialTodos]);
 	
 	function capitalizeFirstLetter(string) {
+    if (!string) {
+        return ''; 
+		}
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
 
 	return (
 		<>
